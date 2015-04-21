92eb3a596b475c2002b4ac5d67fb0eed7cde066a
Rasmus Olesen <rassko@live.com>
2015-04-21 16:25:16 +0200

    Movement
    Movement for the text with arrow keys

--- src/example/SimpleSlickGame.java
+++ src/example/SimpleSlickGame.java
@@ -1,29 +1,59 @@
 package example;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 public class SimpleSlickGame extends BasicGame
 {
+	float x = 200.0f;
+	float y = 200.0f;
+	float speed = 0.2f;
+	
 	public SimpleSlickGame(String gamename)
 	{
 		super(gamename);
 	}
 
 	@Override
-	public void init(GameContainer gc) throws SlickException {}
+	public void init(GameContainer gc) throws SlickException {
+		
+	}
 
 	@Override
-	public void update(GameContainer gc, int i) throws SlickException {}
+	public void update(GameContainer gc, int i) throws SlickException {
+		Input input = gc.getInput();
+		
+		//move right
+		if (input.isKeyDown(Input.KEY_RIGHT)){ 
+			x += speed*i;
+		}
+		
+		//move left
+		if (input.isKeyDown(Input.KEY_LEFT)){
+			x -= speed*i;
+		}
+		
+		//move up
+		if (input.isKeyDown(Input.KEY_UP)){
+			y -= speed*i;
+		}
+		
+		//move down
+		if (input.isKeyDown(Input.KEY_DOWN)){
+			y += speed*i;
+		}
+	}
 
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
-		g.drawString("Hello World!", 250, 200);
+		g.drawString("Hello World!", x, y);
 	}
 
 	public static void main(String[] args)
